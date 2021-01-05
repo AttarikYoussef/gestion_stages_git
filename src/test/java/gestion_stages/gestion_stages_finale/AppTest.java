f490ddd759e10d7aa80c647dc0501da97b3895f3
el malki nassim <nassim_big15@live.fr>
2021-01-05 21:42:10 +0000

    premier commit

---
+++ src/test/java/gestion_stages/gestion_stages_finale/AppTest.java
@@ -0,0 +1,20 @@
+package gestion_stages.gestion_stages_finale;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
